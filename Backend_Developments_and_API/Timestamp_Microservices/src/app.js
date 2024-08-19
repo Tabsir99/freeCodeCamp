c5c569cebcacf76759c6705a2253ca26aee57968
Tabsir Ahammded <tabsircg@gmail.com>
2024-08-19 09:51:28 +0000

    feat: Add cors and allow freecodecamp to make requests to the api

--- Backend_Developments_and_API/Timestamp_Microservices/src/app.js
+++ Backend_Developments_and_API/Timestamp_Microservices/src/app.js
@@ -1,10 +1,15 @@
 import express from "express";
+import cors from 'cors'
+
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cors({
+  origin: 'https://www.freecodecamp.org'
+}))
 
 app.get("/", async (req, res) => {
   res.json({ message: "yep its doing it" });
